2ac0527572951b2e74d8f55dada7d615a5af231e
Drew Brokamp <drew.brokamp@gmail.com>
2021-08-18 19:16:03 -0400

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Drew Brokamp on 8/18/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
